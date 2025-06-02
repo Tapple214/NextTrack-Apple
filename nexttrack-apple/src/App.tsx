54b91dd07388a76a2eac4e3c4d9ef873af2b4289
Tapple214 <applegate21441@gmail.com>
2025-06-02 23:19:01 +0800

    layout

--- nexttrack-apple/src/App.tsx
+++ nexttrack-apple/src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="row mx-2" style={{ height: "100vh" }}>
+      <div className="row m-0 col-12">
+        <div className="bg-primary col-2">1</div>
+        <div className="bg-secondary col-5">2</div>
+        <div className="bg-success col-5">3</div>
+      </div>
+      <div className="row m-0">
+        <div className="bg-danger col-6">4</div>
+        <div className="bg-warning col-6">5</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
