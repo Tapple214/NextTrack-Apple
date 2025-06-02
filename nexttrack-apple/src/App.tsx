16b12cf40904ec8b54ae2b24700049f005aa1465
Tapple214 <applegate21441@gmail.com>
2025-06-03 00:01:51 +0800

    commented div names

--- nexttrack-apple/src/App.tsx
+++ nexttrack-apple/src/App.tsx
@@ -9,18 +9,23 @@ function App() {
     >
       <div className="d-flex col-6" style={{ height: "50%", width: "100vw" }}>
         <div className="d-flex flex-column flex-grow-1">
+          {/* Tools */}
           <div className="bg-primary flex-grow-1" style={{ height: "10%" }}>
             1
           </div>
+          {/* Preview */}
           <div className="bg-secondary flex-grow-1" style={{ height: "80%" }}>
             2
           </div>
         </div>
+        {/* Recommendation Results */}
         <div className="bg-success flex-grow-1">3</div>
       </div>
 
       <div className="d-flex col-6" style={{ height: "50%", width: "100vw" }}>
+        {/* Ready-made Track List */}
         <div className="bg-danger flex-grow-1">4</div>
+        {/*  Custom Track List */}
         <div className="bg-warning flex-grow-1">5</div>
       </div>
     </div>
