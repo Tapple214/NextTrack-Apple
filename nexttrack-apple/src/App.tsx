6854d378189889a890a7987c409b53e15849bce4
Tapple214 <applegate21441@gmail.com>
2025-06-02 23:47:40 +0800

    redid layout

--- nexttrack-apple/src/App.tsx
+++ nexttrack-apple/src/App.tsx
@@ -3,15 +3,25 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="row mx-2" style={{ height: "100vh" }}>
-      <div className="row m-0 col-12">
-        <div className="bg-primary col-2">1</div>
-        <div className="bg-secondary col-5">2</div>
-        <div className="bg-success col-5">3</div>
+    <div
+      className="d-flex flex-column"
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <div className="d-flex col-6" style={{ height: "50%", width: "100vw" }}>
+        <div className="d-flex flex-column flex-grow-1">
+          <div className="bg-primary flex-grow-1" style={{ height: "10%" }}>
+            1
+          </div>
+          <div className="bg-secondary flex-grow-1" style={{ height: "80%" }}>
+            2
+          </div>
+        </div>
+        <div className="bg-success flex-grow-1">3</div>
       </div>
-      <div className="row m-0">
-        <div className="bg-danger col-6">4</div>
-        <div className="bg-warning col-6">5</div>
+
+      <div className="d-flex col-6" style={{ height: "50%", width: "100vw" }}>
+        <div className="bg-danger flex-grow-1">4</div>
+        <div className="bg-warning flex-grow-1">5</div>
       </div>
     </div>
   );
