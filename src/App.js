68e2dd28e81b0d3b8426d0eb9e0ee8aa2e97cfb8
Tapple214 <applegate21441@gmail.com>
2025-07-28 04:00:45 +0800

    creation section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import Item from "./components/ItemDisplay.js";
+import { Button, Popover, OverlayTrigger } from "react-bootstrap";
 
 import recommenderAPI from "./utils/RecommenderAPI.js";
 import ToolsArea from "./components/ToolsArea.js";
@@ -101,15 +102,40 @@ function App() {
 
         {/* Create Custom Playlist */}
         <div
-          className="sections w-50 d-flex text-center justify-content-center align-items-center mt-2 ms-2 me-4 rounded-4 bg-danger"
+          className="sections w-50 d-flex text-center justify-content-center align-items-center mt-2 ms-2 me-4 rounded-4"
           style={{ marginBottom: "45px" }}
         >
-          <div className="mx-3 p-3 rounded mb-3 rounded-2 position-absolute bottom-0 w-50 bg-success">
-            <div
-              className="d-flex justify-content-end gap-2"
-              style={{ backgroundColor: "#f1c28e", width: "70%" }}
-            >
-              <div style={{ color: "#312c51" }}> hello </div>
+          <div className="mx-4 px-3 rounded mb-3 rounded-2 position-absolute bottom-0 w-50">
+            <div className="d-flex justify-content-center">
+              <div
+                className="d-flex justify-content-center gap-2 rounded-3 mb-2"
+                style={{ backgroundColor: "#f1c28e", width: "80%" }}
+              >
+                <div
+                  className="p-3 mx-2 d-flex justify-content-between align-items-center w-100"
+                  style={{ color: "#312c51" }}
+                >
+                  <div className="">
+                    <i class="bi bi-pencil-square"></i>
+                    <input
+                      type="text"
+                      className="input ms-2"
+                      placeholder="Name your tracklist!"
+                    ></input>
+                  </div>
+                  <div>
+                    <Button
+                      variant="link"
+                      className="clickable-icon"
+                      id="icon-btn"
+                      // onClick={}
+                      title="Copy link to clipboard"
+                    >
+                      <i class="bi bi-upload"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
