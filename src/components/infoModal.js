9fbfab0f1373e0425e4e059c34105e0999c1dd88
Tapple214 <applegate21441@gmail.com>
2025-08-07 18:30:28 +0800

    Update infoModal.js

--- src/components/infoModal.js
+++ src/components/infoModal.js
@@ -5,9 +5,17 @@ export default function InfoModal({ message, show, setShow }) {
   return (
     <>
       {show && (
-        <div className="w-100 h-100 bg-black position-absolute bg-opacity-50 d-flex justify-content-center align-items-center">
+        <div
+          className="w-100 h-100 bg-black d-flex justify-content-center align-items-center bg-opacity-50"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            zIndex: 1000,
+          }}
+        >
           <div
-            className="p-4 w-75 rounded-3 d-flex flex-column justify-content-center align-items-center"
+            className="p-4 w-75 rounded-3 d-flex flex-column justify-content-center text-white align-items-center"
             style={{ backgroundColor: "#48426d" }}
           >
             {message}
